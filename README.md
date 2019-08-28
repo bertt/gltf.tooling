68dfd77660db36203649f8cb3be09090ac19397f
bertt <bert.temme@geodan.nl>
2019-08-28 14:37:54 +0200

    initial

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # gltf.tooling
+
+Global tooling for getting detailed (like vertice position, normals and attributes) information about glTF files
+
+## Installation
+
+- Install from NuGet
+
+https://www.nuget.org/packages/gltf.tooling/
+
+```
+$ dotnet tool install -g gltf.tooling
+```
+
+or update:
+
+```
+$ dotnet tool update -g gltf.tooling
+
+```
+
+## Running
+
+1] Command Info gltf_file 
+
+Example:
+
+```
+$ gltf info test.glb
+
+File: test.glb
+Generator: SharpGLTF 1.0.0
+Version: 2.0
+Primitive type: TRIANGLES
+Vertex accessors: POSITION,NORMAL,_CUSTOM_1,COLOR_0,TEXCOORD_0
+Vertex accessor: POSITION
+4373.1924, 5522.678, -359.8238
+4370.978, 5522.723, -359.89185
+4364.6157, 5511.5107, -359.08923
+Vertex accessor: NORMAL
+0.57735026, 0.57735026, 0.57735026
+0.57735026, 0.57735026, 0.57735026
+0.57735026, 0.57735026, 0.57735026
+Vertex accessor: _CUSTOM_1
+100
+101
+102
+Vertex accessor: COLOR_0
+1,1, 1, 1
+1,1, 1, 1
+1,1, 1, 1
+Vertex accessor: TEXCOORD_0
+texcoord_0
+0, 0
+0, 0
+0, 0
+```
---
+++ src/Program.cs
@@ -0,0 +1,91 @@
+﻿using SharpGLTF.Schema2;
+using System;
+using System.Reflection;
+
+namespace gltf.tooling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                var versionString = Assembly.GetEntryAssembly()
+                                        .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                                        .InformationalVersion
+                                        .ToString();
+
+                Console.WriteLine($"gltf v{versionString}");
+                Console.WriteLine("-------------");
+                Console.WriteLine("\nUsage:");
+                Console.WriteLine("  gltf info <glb-file>");
+                return;
+            }
+
+            if (args[0] == "info")
+            {
+                Info(args[1]);
+            }
+        }
+
+        private static void Info(string file)
+        {
+            Console.WriteLine("File: " + file);
+            var model = ModelRoot.Load(file);
+            Console.WriteLine("Generator: " + model.Asset.Generator);
+            Console.WriteLine("Version: " + model.Asset.Version);
+
+            foreach(var mesh in model.LogicalMeshes)
+            {
+                foreach(var primitive in mesh.Primitives)
+                {
+                    Console.WriteLine("Primitive type: " + primitive.DrawPrimitiveType);
+
+                    Console.WriteLine("Vertex accessors: " + String.Join(',', primitive.VertexAccessors.Keys));
+
+                    foreach(var vertexAccessor in primitive.VertexAccessors)
+                    {
+                        var va = primitive.GetVertexAccessor(vertexAccessor.Key);
+                        Console.WriteLine("Vertex accessor: " + vertexAccessor.Key);
+
+                        if (vertexAccessor.Key == "POSITION" || vertexAccessor.Key == "NORMAL")
+                        {
+                            var vectors = va.AsVector3Array();
+                            foreach(var v in vectors)
+                            {
+                                Console.WriteLine(v.X + ", "+ v.Y + ", "+ v.Z);
+                            }
+                        }
+                        else if (vertexAccessor.Key == "COLOR_0")
+                        {
+                            var colors = va.AsColorArray();
+                            foreach(var c in colors)
+                            {
+                                Console.WriteLine($"{c.X},{c.Y}, {c.Z}, {c.W}");
+                            }
+                        }
+                        else if (vertexAccessor.Key == "TEXCOORD_0")
+                        {
+                            Console.WriteLine("texcoord_0");
+                            var texcoords = va.AsVector2Array();
+                            foreach(var texcoord in texcoords)
+                            {
+                                Console.WriteLine(texcoord.X + ", " + texcoord.Y);
+                            }
+                        }
+
+                        else if (vertexAccessor.Key.StartsWith("_")){
+                            // custom attribute
+                            var scalars = va.AsScalarArray();
+                            foreach (var s in scalars)
+                            {
+                                Console.WriteLine(s);
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
